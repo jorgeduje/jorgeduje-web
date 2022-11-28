fd22a9579fe40a86aee6ad8e52172c8a5da68668
jorgeduje <julian.russo@team.wirall.com>
2022-11-28 11:18:23 -0300

    puesta a punto

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,38 @@
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardTitle,
+    CardText,
+    CardLink
+  } from "reactstrap"
+  
+
+const Home = () => {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Kick start your project 🚀</CardTitle>
+        </CardHeader>
+        <CardBody>
+          <CardText>All the best for your new project.</CardText>
+          <CardText>
+            Please make sure to read our{" "}
+            <CardLink
+              href="https://pixinvent.com/demo/vuexy-react-admin-dashboard-template/documentation/"
+              target="_blank"
+            >
+              Template Documentation
+            </CardLink>
+            to understand where to go from here and how to use our template.
+          </CardText>
+        </CardBody>
+      </Card>
+
+    
+    </div>
+  )
+}
+
+export default Home
